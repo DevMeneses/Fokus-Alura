2205745af7ae70926ecba4b2648d9db1d83433f4
Matheus <menesesmatheus993@gmail.com>
2025-09-24 12:38:21 -0300

    Alterando texto da página

--- script.js
+++ script.js
@@ -27,8 +27,32 @@ longoBt.addEventListener('click', () => {
   alterarContexto('descanso-longo')
 })
 
-function alterarContexto(contexto){
+function alterarContexto(contexto) {
   html.setAttribute('data-contexto', contexto)
   banner.setAttribute('src', `/imagens/${contexto}.png`)
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML =
+        `
+        Otimize sua produtividade, <br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong> 
+        `
+      break;
+    case "descanso-curto":
+      titulo.innerHTML =
+        `
+        Que tal dar uma respirada? <br>
+        <strong class="app__title-strong">Faça uma pausa curta!.</strong> 
+        `
+      break;
+    case "descanso-longo":
+      titulo.innerHTML =
+        `
+        Hora de voltar à superfície. <br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong> 
+        `
+    default:
+      break;
+  }
 }
 
